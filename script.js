6eb99ff90492cbb079f2a235ef150344846fb737
BryantNorman <bnorman.codetta@gmail.com>
2024-01-19 19:38:06 +0530

    Added Vertical Skew

--- script.js
+++ script.js
@@ -3,22 +3,23 @@ document.addEventListener("DOMContentLoaded", function () {
     const outputText1 = document.getElementById("outputText1");
     const spacingSlider = document.getElementById("spacingSlider");
     const widthSlider = document.getElementById("widthSlider");
+    const heightSlider = document.getElementById("heightSlider");
 
     textInput.addEventListener("input", updateOutputText);
     spacingSlider.addEventListener("input", updateOutputText);
     widthSlider.addEventListener("input", updateOutputText);
+    heightSlider.addEventListener("input", updateOutputText);
 
     function updateOutputText() {
         const inputValue = textInput.value;
         const spacingValue = spacingSlider.value;
-
-        const textWidth = inputValue.length * 20;
-
-        outputText1.style.width = textWidth + "px";
-
         const widthValue = widthSlider.value;
-        const scale = widthValue / 400;
-        outputText1.style.transform = `scaleX(${scale})`;
+        const heightValue = heightSlider.value;
+
+        const scaleX = widthValue / 400;
+        const scaleY = heightValue / 200;
+
+        outputText1.style.transform = `scaleX(${scaleX}) scaleY(${scaleY})`;
 
         outputText1.textContent = inputValue.replace(/./g, (char) => {
             return char + "\u200A".repeat(spacingValue);
